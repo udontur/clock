ac54efb076d0968f33d5050bfcaf2e84174af34d
Hadrian <118540129+udontur@users.noreply.github.com>
2024-06-30 04:36:08 +0000

    Change: Font changed to M PLUS Rounded 1c

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,7 @@ module.exports = {
   content: ["./index.html", "./404.html", "./clock.html"],
   theme: {
     extend: {
-      fontFamily: {
-        jetmono: ["JetBrains Mono", "monospace"],
-      },
+      
     },
   },
   plugins: [],
