11f6ea3f4937a3ff9a106b7e3f469fbe9b34749a
udontur <h@hadrianlau.com>
2024-12-27 20:40:50 +0800

    add permalink

--- learn/CodingPlayground/script.js
+++ learn/CodingPlayground/script.js
@@ -1,21 +1,21 @@
-let listOf=document.querySelector("ul");
-let addButton=document.querySelector("button");
-let input1=document.querySelector("input");
+function multiplyNumeric(obj) {
+    for(let key in obj){
+        if(typeof obj[key] === "number"){
+            obj[key]*=2;
+        }
+    }
+}
 
-addButton.addEventListener("contextmenu", (e)=>{
-    e.preventDefault();
-});
-addButton.addEventListener("click",()=>{
-    let newDiv=document.createElement("li");
-    newDiv.textContent=input1.value;
-    if(input1.value==="") return;
-    input1.value="";
-    let rmBut=document.createElement("button");
-    rmBut.textContent="Remove"
-    rmBut.classList.add("removeBut");
-    newDiv.appendChild(rmBut);
-    listOf.appendChild(newDiv);
-    rmBut.addEventListener("click",()=>{
-        newDiv.remove();
-    });
-});
+let menu = {
+    width: 200,
+    height: 300,
+    title(){
+        console.log(`My menu is \$${this.width}`);
+    },
+};
+
+multiplyNumeric(menu);
+
+console.log(menu)
+
+menu.title();
